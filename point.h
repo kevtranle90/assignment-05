450302eb9ca010a01f3be6982241df4a821e615d
kevtranle90 <kevintran323@csu.fullerton.edu>
2016-05-03 03:02:48 -0700

    finished assignment

---
+++ point.h
@@ -0,0 +1,29 @@
+/* ----------------------------------------------------------------------------
+ *Copyright &copy; 2016 YOUR_NAME <kevintran323@csu.fullerton.edu>
+ *Released under the[MIT License](http://opensource.org/licenses/MIT)
+ *------------------------------------------------------------------------ - */
+
+/**
+ * A class representing a point in a Euclidean plane.
+ */
+
+#ifndef POINT_H
+#define POINT_H
+
+class Point {
+    private:
+        float x;
+        float y;
+
+    public:
+        Point(float x = 0, float y = 0);
+
+        float getX() const;
+        float getY() const;
+
+        float distance(const Point & a) const;
+};
+
+// ----------------------------------------------------------------------------
+#endif  // POINT_H
+
---
+++ shapes.h
@@ -0,0 +1,57 @@
+/* ----------------------------------------------------------------------------
+ *Copyright &copy; 2016 YOUR_NAME <kevintran323@csu.fullerton.edu>
+ *Released under the[MIT License](http://opensource.org/licenses/MIT)
+ *------------------------------------------------------------------------ - */
+
+
+#ifndef SHAPES_H
+#define SHAPES_H
+#include "point.h"
+
+class Shape {
+    public:
+        virtual ~Shape() {}
+        virtual bool contains(const Point & p) const = 0;  // pure virtual
+};
+
+// ----------------------------------------------------------------------------
+
+class Rectangle : public Shape {
+		private:
+        Point center;
+        float dx;
+        float dy;
+
+    public:
+        Rectangle(Point center = Point(), float dx = 0, float dy = 0);
+
+        bool contains(const Point & p) const;
+};
+
+class Square : public Rectangle {
+    public:
+        Square(Point center = Point(), float side = 0);
+};
+
+// ----------------------------------------------------------------------------
+
+class Ellipse : public Shape {
+    private:
+        Point f1;
+        Point f2;
+        float radius;
+
+    public:
+        Ellipse(Point f1 = Point(), Point f2 = Point(), float radius = 0);
+
+        bool contains(const Point & p) const;
+};
+
+class Circle : public Ellipse {
+    public:
+        Circle(Point center = Point(), float radius = 0);
+};
+
+// ----------------------------------------------------------------------------
+#endif  // SHAPES_H
+
